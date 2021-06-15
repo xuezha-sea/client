e3719174fdee8f02a71ab0a992cacd5389422f76
XueZha.me <i@xuezha.me>
2021-06-15 15:49:20 +0800

    add assets

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+import 'data/repository/account_repository.dart';
+import 'logic/account/account_cubit.dart';
+import 'ui/app_route.dart';
+import 'ui/theme.dart';
 
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+    AccountRepository repository = AccountRepository();
 
+    return MaterialApp(
+      initialRoute: AppRoute.home,
+      routes: AppRoute.routes,
+      navigatorKey: AppRoute.navigatorKey,
+      theme: theme,
+    ).parent(({required child}) => BlocProvider(
+          create: (context) => AccountCubit(repository: repository),
+          child: child,
+        ));
+  }
 }
